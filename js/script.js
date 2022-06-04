2f231feae199a5bc34270f98a138d9aadeb1c786
VladPas <paskinvw@gmail.com>
2022-06-04 16:47:07 +0300

    trololo

---
+++ js/script.js
@@ -0,0 +1,55 @@
+"use strict";
+
+
+//dz1
+let nameAdmin = ("Джон");
+let nameUser = nameAdmin;
+alert ('name you' + nameAdmin );
+
+
+//alert ('Hello');
+
+//const result = confirm("Are you here?");
+//console.log(result);
+
+//const answer = prompt("Вам есть 18?","18");
+//console.log(answer+5);
+
+//const answers = [];
+//answers [0]= prompt('Как ваше имя?','');
+//answers [1]= prompt('Как ваша Фамилия?','');
+//answers [2]= prompt('Сколько вам лет?','');
+//
+//document.write(answers);
+//console.log(typeof(answers));
+//console.log(typeof(answers));
+
+// интерполяция
+//const category = "toys";
+//console.log(`https://someurl.com/${category}/5`);
+//
+//const user = "vlad";
+//
+//alert(`Привет, ${user}`);
+
+//конкатинация
+//console.log ("arr"+"- object");
+//console.log ("4"+"- object");
+//console.log (4 + + "5");
+
+
+//инкремент - декримент
+//let incr = 10,
+//decr = 10;
+////++incr;
+////--decr;
+//console.log(++incr);
+//console.log(--decr);
+//console.log(5%2);
+//console.log(2*4 === 8);
+//&& и
+//|| или
+// const isChecked = false,
+// isclose = false;
+// console.log(isChecked && !isclose);
+
